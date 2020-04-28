b5b043c8115907ac4e90508f86c7c6bde4d778c9
Ilmira Ism <ilmira.ismilyaeva@yandex.ru>
2020-04-28 14:15:58 +0300

    finish version of mesto

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let prInfo = document.querySelector('.profile__info');
-let editBtn = prInfo.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-let closeBtn = popup.querySelector('.popup__btn-close');
-let saveBtn = popup.querySelector('.popup__btn-save');
-let formElement = popup.querySelector('.popup__container');
+const prInfo = document.querySelector('.profile__info');
+const editBtn = prInfo.querySelector('.profile__edit');
+const popup = document.querySelector('.popup');
+const closeBtn = popup.querySelector('.popup__btn-close');
+const saveBtn = popup.querySelector('.popup__btn-save');
+const formElement = popup.querySelector('.popup__container');
 let prTitle = document.querySelector('.profile__title');
 let prSubtitle = document.querySelector('.profile__subtitle');
 
-// 1. задать функцию всплывающее окно
 function showPopup() {
     popup.classList.toggle('popup_opened');
-    // 1.1 сохранить существующее значение
+  
     let nameInput = popup.querySelector('.popup__text_type_name');
     nameInput.value = prTitle.textContent;
 
@@ -19,17 +18,14 @@ function showPopup() {
 }
 editBtn.addEventListener('click', showPopup);
 
-//2. задать функцию закрыть окно
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 closeBtn.addEventListener('click', closePopup);
-// задать функцию данные формы
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
-
-    // 1. ввести новые данные
-    // 1.1. возьмем новое имя
+ 
     let newName = popup.querySelector('.popup__text_type_name').value;
     console.log('New name of Jack Custo is');
     console.log(newName);
@@ -37,17 +33,13 @@ function formSubmitHandler(evt) {
     let newJob = popup.querySelector('.popup__text_type_job').value;
     console.log('New job of исследователь is');
     console.log(newJob);
-    // 1.2. сохраним новое имя на странице
+   
     let profileName = document.querySelector('.profile__title');
     profileName.textContent = newName;
 
     let profileJob = document.querySelector('.profile__subtitle');
     profileJob.textContent = newJob;
-    
-    // 1.3. возьмем новый род деят-ти
-    // 1.4. сохраним новый род деятель-ти
-    // 2. закрыть popup
-  
+   
     closePopup();
 }
 formElement.addEventListener('submit', formSubmitHandler);
